212f3979c5bb013325461fb84fda9b618c8a708b
yousseffatach <youssef.allinlab@gmail.com>
2024-11-09 18:19:15 +0100

    Added models to showcase ForeignKey, ManyToMany, and OneToOne relationships

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +14,11 @@ urlpatterns = [
     
     # URL for the class-based view (library details)
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    
+    # URL for logout (using Django's built-in LogoutView)
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    
+    # URL for registration (using the custom register view)
+    path('register/', views.register, name='register'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,6 +6,24 @@ from .models import Library
 # Create your views here.
 from .models import Book
 
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the user to the database
+            return redirect('login')  # Redirect to the login page after successful registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+
+
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()  # Fetch all books from the database
