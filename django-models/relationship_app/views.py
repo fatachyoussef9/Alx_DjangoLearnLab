97c9c62a59935ba5560fa0fb77a11f132d2a31f4
yousseffatach <youssef.allinlab@gmail.com>
2024-11-09 18:21:00 +0100

    Added models to showcase ForeignKey, ManyToMany, and OneToOne relationships

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 from .models import Library
 # Create your views here.
 from .models import Book
-
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth import registration
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LogoutView
