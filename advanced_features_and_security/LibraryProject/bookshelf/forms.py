c3ca58b07446e901ddcbd9b77801863bf3fbefbe
yousseffatach <youssef.allinlab@gmail.com>
2024-11-14 11:49:17 +0100

    Added models to showcase ForeignKey, ManyToMany, and OneToOne relationships

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,9 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author']
+
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book  # Specify the model you're working with (e.g., Book)
+        fields = ['title', 'author']
