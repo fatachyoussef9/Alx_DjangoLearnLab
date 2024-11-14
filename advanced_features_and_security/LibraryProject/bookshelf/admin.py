ccbb4c99757ba27cd52db2534b95373e5a3bf68e
yousseffatach <youssef.allinlab@gmail.com>
2024-11-14 11:12:36 +0100

    Added models to showcase ForeignKey, ManyToMany, and OneToOne relationships

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
 
 # Register the Book model with custom admin settings
 @admin.register(Book)
@@ -7,3 +12,16 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')  # Columns to display in the admin list view
     search_fields = ('title', 'author')  # Fields to search by
     list_filter = ('publication_year',)  # Filter by publication year
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+# admin.site.register(CustomUser, CustomUserAdmin)
+["admin.site.register(CustomUser, CustomUserAdmin)"]
