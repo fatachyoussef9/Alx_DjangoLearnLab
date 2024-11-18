24183d7e7086434e1c1fae0206323f957af9d3b6
yousseffatach <youssef.allinlab@gmail.com>
2024-11-18 19:53:13 +0100

    Added models to showcase ForeignKey, ManyToMany, and OneToOne relationships

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -7,4 +7,6 @@ class BookList(ListAPIView):
     queryset = Book.objects.all()  # Retrieve all book instances
     serializer_class = BookSerializer
 
+
+["generics.ListAPIView"]
 # Create your views here.
