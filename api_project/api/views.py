785630559a8a6d71fed0191a44e1a0d61e1bd65c
yousseffatach <youssef.allinlab@gmail.com>
2024-11-18 20:04:44 +0100

    Added models to showcase ForeignKey, ManyToMany, and OneToOne relationships

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
-
+from rest_framework.permissions import IsAuthenticated
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()  # Retrieve all book instances
@@ -17,3 +17,11 @@ class BookList(ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can access these views
+
