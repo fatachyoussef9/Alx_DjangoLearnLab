4afb724aaa90c305587026aaedd6391460828097
yousseffatach <youssef.allinlab@gmail.com>
2024-11-30 18:15:53 +0100

    Added models to showcase ForeignKey, ManyToMany, and OneToOne relationships

--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -3,11 +3,20 @@ from .models import Book
 from .serializers import BookSerializer
 
 from rest_framework.permissions import AllowAny
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
+ 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [AllowAny]  # Allow all users to view the book list
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['title', 'author__name', 'publication_year']  # Fields for filtering
+    search_fields = ['title', 'author__name']  # Fields for searching
+    ordering_fields = ['title', 'publication_year']  # Fields for ordering
+    ordering = ['title']  # Default ordering
+
 
 # DetailView - Retrieve a single book by ID
 class BookDetailView(generics.RetrieveAPIView):
