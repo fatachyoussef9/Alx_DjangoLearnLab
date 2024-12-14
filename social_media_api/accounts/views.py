d9e39a284d95905f2164f61a39e0307f95a497b9
yousseffatach <youssef.allinlab@gmail.com>
2024-12-14 08:46:06 +0100

    Added models to showcase ForeignKey, ManyToMany, and OneToOne relationships

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -31,26 +31,67 @@ def login(request):
 
 
 
+# @api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+# def follow_user(request, user_id):
+#     try:
+#         user_to_follow = CustomUser.objects.get(id=user_id)
+#         if user_to_follow != request.user:
+#             request.user.following.add(user_to_follow)
+#             return Response({'message': 'You are now following this user.'}, status=status.HTTP_200_OK)
+#         return Response({'error': 'You cannot follow yourself.'}, status=status.HTTP_400_BAD_REQUEST)
+#     except CustomUser.DoesNotExist:
+#         return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+# @api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+# def unfollow_user(request, user_id):
+#     try:
+#         user_to_unfollow = CustomUser.objects.get(id=user_id)
+#         if user_to_unfollow in request.user.following.all():
+#             request.user.following.remove(user_to_unfollow)
+#             return Response({'message': 'You have unfollowed this user.'}, status=status.HTTP_200_OK)
+#         return Response({'error': 'You are not following this user.'}, status=status.HTTP_400_BAD_REQUEST)
+#     except CustomUser.DoesNotExist:
+#         return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+
+
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from .models import CustomUser
+
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def follow_user(request, user_id):
+    """
+    Allows the authenticated user to follow another user.
+    """
     try:
-        user_to_follow = CustomUser.objects.get(id=user_id)
-        if user_to_follow != request.user:
-            request.user.following.add(user_to_follow)
-            return Response({'message': 'You are now following this user.'}, status=status.HTTP_200_OK)
-        return Response({'error': 'You cannot follow yourself.'}, status=status.HTTP_400_BAD_REQUEST)
+        user_to_follow = CustomUser.objects.get(pk=user_id)
+        if user_to_follow == request.user:
+            return Response({'error': 'You cannot follow yourself.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        request.user.following.add(user_to_follow)
+        return Response({'message': f'You are now following {user_to_follow.username}.'}, status=status.HTTP_200_OK)
     except CustomUser.DoesNotExist:
         return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def unfollow_user(request, user_id):
+    """
+    Allows the authenticated user to unfollow another user.
+    """
     try:
-        user_to_unfollow = CustomUser.objects.get(id=user_id)
+        user_to_unfollow = CustomUser.objects.get(pk=user_id)
         if user_to_unfollow in request.user.following.all():
             request.user.following.remove(user_to_unfollow)
-            return Response({'message': 'You have unfollowed this user.'}, status=status.HTTP_200_OK)
+            return Response({'message': f'You have unfollowed {user_to_unfollow.username}.'}, status=status.HTTP_200_OK)
+
         return Response({'error': 'You are not following this user.'}, status=status.HTTP_400_BAD_REQUEST)
     except CustomUser.DoesNotExist:
         return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
