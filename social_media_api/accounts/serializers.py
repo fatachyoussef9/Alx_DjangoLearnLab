3a51def800e2258d4bb2bd31da5e381254a2d0ce
yousseffatach <youssef.allinlab@gmail.com>
2024-12-14 08:22:24 +0100

    Added models to showcase ForeignKey, ManyToMany, and OneToOne relationships

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,12 +1,26 @@
 from rest_framework import serializers
-from .models import CustomUser
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(read_only=True)
+
     class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'password']
+        model = User
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'password', 'token']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)
+        # Create the user
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', None),
+            password=validated_data['password']
+        )
+        # Create an authentication token for the user
+        token, created = Token.objects.get_or_create(user=user)
+        # Attach the token to the serialized response
+        user.token = token.key
         return user
