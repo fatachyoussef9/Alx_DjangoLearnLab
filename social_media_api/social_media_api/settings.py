639afa34d29b414de5c71374698a1ec089529230
yousseffatach <youssef.allinlab@gmail.com>
2024-12-14 09:10:33 +0100

    Added models to showcase ForeignKey, ManyToMany, and OneToOne relationships

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,11 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-y@p!vwwlu51$6^n45(#)1d!cn_n7+mh_=ms6pwyg!vf(em#s@x'
+#  SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['yourdomain.com', 'www.yourdomain.com']
 
 
 # Application definition
@@ -81,6 +87,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': os.environ.get('DB_USER'),        # Database user
+        'PASSWORD': os.environ.get('DB_PASSWORD'),# Database password
+        'HOST': os.environ.get('DB_HOST', 'localhost'),  # Database host, 'localhost' is default
+        'PORT': os.environ.get('DB_PORT', '3306'), 
     }
 }
 
@@ -131,3 +141,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
+
+
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True  # Redirect all traffic to HTTPS
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_BUCKET_NAME')
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
